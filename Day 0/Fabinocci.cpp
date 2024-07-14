45255ae91b4c8185f8542dc1f4c311efe0a82845
Khuram gill <khuramiqbalofficial@gmail.com>
2024-07-14 23:53:00 +0500

    .

--- Day 0/Fabinocci.cpp
+++ Day 0/Fabinocci.cpp
@@ -1,36 +1,26 @@
-#include<iostream>
-using namespace std;
-// 
-int peakIndexInMountainArray(vector<int>& arr) {
-        int strt = 0;
-        int end = arr.size() - 1;
-        int mid = (strt + end)/2;
-        while(strt<end){
-            if(arr[mid] < arr[mid+1]){
-                strt = mid+1;
-            }
-            else{
-                end = mid;
-            }
-            mid = strt + (end-strt)/2;
+#include <iostream>
+#include <vector>
+#include <numeric> // For std::accumulate
 
-        }
-        return strt;
-        
-    }
-main(){
-    // Range of int is -2^31 to 2^31-1
-    //fabinocci series generater code
-    int n;
-    cin>>n;
-    int a=0,b=1;
-    cout<<a<<" "<<b<<" ";
-    for(int i=2;i<n;i++){
-        int c=a+b;
-        cout<<c<<" ";
-        a=b;
-        b=c;
-    }
-    cout<<endl;
+using namespace std;
+
+int pivotIndex(vector<int>& nums) {
+    int total_sum = accumulate(nums.begin(), nums.end(), 0);
+    int left_sum = 0;
     
+    for (int i = 0; i < nums.size(); ++i) {
+        if (left_sum == total_sum - left_sum - nums[i]) {
+            return i;
+        }
+        left_sum += nums[i];
+        // cout << "Left Sum: " << left_sum << endl;
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {1, 7, 3, 6, 5, 6};
+    int result = pivotIndex(arr);
+    cout << "Pivot Index is: " << result << endl;
+    return 0;
 }
